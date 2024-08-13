755ecbcc35328941913e66cab78a48d4e0e78c57
Andrea Melissa Alvarez Herrera <andrea.alvarez@senprende.hn>
2024-08-13 16:48:08 -0600

    Video numero 63

--- js/dom.js
+++ js/dom.js
@@ -31,4 +31,27 @@
 
 /** */
 console.log(document.documentElement.lang);
-console.log(document.documentElement.getAttribute("link-dom"));
+//console.log(document.documentElement.getAttribute("link-dom"));
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+
+const $linkDOM = document.querySelector(".link-dom"); 
+
+$linkDOM.setAttribute("target", "_blank"); 
+$linkDOM.setAttribute("rel", "noopener"); 
+$linkDOM.setAttribute("href", "https://correo.senprende.hn/?loginOp=logout"); 
+
+
+/** DATA-ATTRIBUTES */ //video 63
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo del Objeto del Documento"); 
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Revisa tu correo del trabajo"; 
+console.log($linkDOM.dataset.description);
+console.log($linkDOM.hasAttribute("data-id"));
+console.log($linkDOM.removeAttribute("data-id"));
+
